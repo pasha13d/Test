da769e14e6953feb540d22c04160e516a968ede5
pasha13d <mahabub13d1995@gmail.com>
2021-09-09 11:08:23 +0600

    file upload

---
+++ Test/Controllers/FileController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class FileController : Controller
+    {
+        //private IHostingEnvironment environment;
+        //public FileController(IHostingEnvironment _environment)
+        //{
+        //    environment = _environment;
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fileupload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Fileupload(IFormFile file)
+        {
+            FileUploadViewModel fileUpload = new FileUploadViewModel();
+            //Filesize
+            fileUpload.FileSize = 550;
+            bool result = fileUpload.UploadUserFile(file);
+
+            if (result)
+            {
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UploadFile"));
+                //Declare path to save uploaded file
+                var path = Path.Combine(
+                            Directory.GetCurrentDirectory(), "wwwroot", "UploadFile",
+                            file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            ViewBag.ResultErrorMessage = fileUpload.Message;
+
+            return View();
+        }
+
+        #region Private Methods
+
+        #endregion
+    }
+}
---
+++ Test/Models/FileUploadViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class FileUploadViewModel
+    {
+        public string Message { get; set; }
+        public decimal FileSize { get; set; }
+        public bool UploadUserFile([FromForm] IFormFile file)
+        {
+            bool result = false;
+            try
+            {
+                //var supportedTypes = new[] { "txt", "doc", "docx", "pdf", "xls", "xlsx" };
+                //var fileExt = System.IO.Path.GetExtension(file.FileName).Substring(1);
+                //if (!supportedTypes.Contains(fileExt))
+                //{
+                //    Message = "Invalid file extension - uploads word/pdf/excel/txt file only";
+                //    return Message;
+                //}
+                //Mime content type for supported files -- pdf, word, excel, text
+                bool isValidFileType = string.Equals(file.ContentType, "application/pdf", StringComparison.OrdinalIgnoreCase) ||
+                                    string.Equals(file.ContentType, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", StringComparison.OrdinalIgnoreCase) ||
+                                    string.Equals(file.ContentType, "application/vnd.ms-excel", StringComparison.OrdinalIgnoreCase) ||
+                                    string.Equals(file.ContentType, "text/plain", StringComparison.OrdinalIgnoreCase) ||
+                                    string.Equals(file.ContentType, "application/msword", StringComparison.OrdinalIgnoreCase) ||
+                                    string.Equals(file.ContentType, "application/vnd.openxmlformats-officedocument.wordprocessingml.document", StringComparison.OrdinalIgnoreCase);
+                if (!isValidFileType)
+                {
+                    Message = "Invalid file extension - uploads word/pdf/excel/txt file only";
+                }
+                else if (file.Length > (FileSize * 1024))
+                {
+                    Message = "File size should be upto " + FileSize + "KB";
+                }
+                else
+                {
+                    Message = "File Is Successfully Uploaded";
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = "Upload Container Should Not Be Empty";
+            }
+            return result;
+        }
+    }
+}
